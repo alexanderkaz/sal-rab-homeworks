62252bb278e8ad648bf937441f36d11124468e0d
alexanderkaz <84435563+alexanderkaz@users.noreply.github.com>
2021-09-08 12:24:54 +0300

    task_final_fix

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,15 +34,14 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client: {}, order: {}, goods: []};
+    let data = {order: {}, goods: []};
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i < countOfGoods; i++ ) {
-        data.goods.push(goods[i].title + goods[i].count);
+    for (let i = 0; i < countOfGoods; i += 1 ) {
+        data.goods.push ({title: goods[i].title, count: goods[i].count});
     }
    
-    
     data.order.address = "ул. " + address.street + ", дом " + address.house + ", " +address.entrance + " подъезд, " + address.floor + " этаж, кв " + address.flat;
     data.order.sum = sum;
 
